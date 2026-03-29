9c6dea6e328e6227d1a75e28abf6e981ddaf6436
Eddie1308 <67546692+Eddie1308@users.noreply.github.com>
2026-03-29 16:30:10 +0300

    Delete clinic_management directory

--- clinic_management/clinic_management/doctype/appointment_installment/appointment_installment.py
+++
@@ -1,5 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-class $(echo $d | sed 's/_/ /g' | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2); print}' | tr -d ' ')(Document):
-    pass
